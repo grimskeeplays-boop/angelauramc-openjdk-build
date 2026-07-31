3af8f9f358ed1cb0ffe896eeac7c4ca3e9a998d3
grimskeeplays-boop <grimskeeplays-boop@users.noreply.github.com>
2026-07-31 08:41:00 -0700

    Build Java 21.0.8 for Android ARM64

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -6,7 +6,7 @@ if [[ $TARGET_VERSION -eq 21 ]]; then
         git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     else
         # TODO: Update Android to JDK 21.0.8+7
-        git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+        git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     fi
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
