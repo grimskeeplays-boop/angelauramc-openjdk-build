8ed0fd38a54e8c34f965929f33028aabf12d0306
grimskeeplays-boop <grimskeeplays-boop@users.noreply.github.com>
2026-08-01 15:01:38 -0700

    JDK21THOR: update OpenJDK base to 21.0.8+9

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -3,10 +3,10 @@ set -e
 
 if [[ $TARGET_VERSION -eq 21 ]]; then
     if [[ $BUILD_IOS ]]; then
-        git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+        git clone --branch jdk-21.0.8+9 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     else
-        # TODO: Update Android to JDK 21.0.8+7
-        git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+        # TODO: Update Android to JDK 21.0.8+9
+        git clone --branch jdk-21.0.8+9 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     fi
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
